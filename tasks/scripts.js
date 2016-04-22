2186711be61bc5946a81ca47dcdb4802c3db4e2d
Jerven Clark Chua <jervenclark@gmail.com>
2016-04-22 15:37:46 +0800

    Add scripts task

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,7 +1,14 @@
 'use strict';
 
-module.exports = function (gulp, config, plugins) {
-  return function () {
-    console.log(plugins)
+module.exports = function(gulp, config, plugins) {
+  return function() {
+    return gulp.src('src/js/**/*.js')
+      .pipe(plugins.jshint.reporter('default'))
+      .pipe(plugins.concat('main.js'))
+      .pipe(gulp.dest('dist/assets/js'))
+      .pipe(plugins.rename({ suffix: '.min' }))
+      .pipe(plugins.uglify())
+      .pipe(gulp.dest('dist/assets/js'))
+      .pipe(plugins.notify({ message: 'Scripts task complete' }));
   }
 }
